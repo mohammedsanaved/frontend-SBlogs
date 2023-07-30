bf84324b7430f60fd1d91685c82517fe50759ab0
Mohammed Sanaved <mosanaved@gmail.com>
2023-07-31 02:16:38 +0530

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col">HOME</div>
-        </div>
-      </div>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
